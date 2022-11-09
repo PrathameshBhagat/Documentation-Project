03e9fdf04b7d1f24bf81c1fcde8a340024f259fb
Your Name <you@example.com>
2022-11-09 17:45:07 +0530

    add side nav,beautify NEXT and update previous

--- scripts/article.js
+++ scripts/article.js
@@ -1,51 +1,99 @@
- 
- function load() {
-//set language to english ,hide hindi
-change('h','none');
-//hide all chapters
-const ch = document.getElementsByTagName("chapter");
+function load() {
+    //set language to english ,hide hindi
+    change('h','none');
+    //hide all chapters
+    const ch = document.getElementsByTagName("chapter");
     for (let i = 0; i < ch.length; i++) {
-      ch[i].style.display = "none";
-}
-//display first chapter and save active chapter index in c
-document.getElementsByTagName("chapter")[0].style.display="block";
+          ch[i].style.display = "none";
+    }
+    //add event to change language on . press 
+    document.getElementsByTagName("body")[0].addEventListener("keydown", dot);
+    //add event to change language on . press only outside all coding area to print "."
+    const cdarea = document.getElementsByClassName("code");
+    for (let i = 0; i < cdarea.length; i++) {
+        cdarea[i].addEventListener("mouseover",revent );
+        cdarea[i].addEventListener("mouseout", aevent);
+    }
+    //display first chapter and save active chapter index in c
+    document.getElementsByTagName("chapter")[0].style.display="block";
 };
 var c=0;
-function change(tag,val)//function to change language
+//function to add event to change la.......nguage on . press 
+function aevent(){document.getElementsByTagName("body")[0].addEventListener("keydown", dot);}
+//function to remove event to change language on . press 
+function revent(){document.getElementsByTagName("body")[0].removeEventListener("keydown", dot);} 
+//function to change display of all given tags via parameters passed
+function change(tag,val)
 {  
     const nodes = document.getElementsByTagName(tag);
     for (let i = 0; i < nodes.length; i++) {
       nodes[i].style.display = val;
     }
 }
-function ehchange(){
+function ehchange(){//check and change language 
         if(document.getElementsByTagName("h")[0].style.display=="none")
             {change("h","block");change("e","none");}
         else if(document.getElementsByTagName("e")[0].style.display=="none")
             {change("e","block");change("h","none");}
 }
-function set(v){if(v=="english"){change("e","block");change("h","none");}
-else {change("h","block");change("e","none");}
+function codeload()//load editor
+{
+const cd = document.getElementsByClassName("code");
+    for (let i = 0; i < cd.length; i++) {
+        let editor = 0;
+        ace.edit(cd[i],{theme:"ace/theme/cobalt",
+        mode:"ace/mode/javaa",
+    });
+    }
+} 
+//change display of indivisual tag
+function set(v){
+    if(v=="english")
+        {change("e","block");change("h","none");}
+    else 
+        {change("h","block");change("e","none");}
 }
- function dot()
-{ var key=('which' in event) ? event.which : event.keyCode;
-   if(key=="190"){
-    if(document.getElementsByTagName("select")[0].selectedIndex==0)
-{document.getElementsByTagName("select")[0].selectedIndex=1;set("hindi");}
-    else {document.getElementsByTagName("select")[0].selectedIndex=0;set("english");}
+function dot()//on click of button change language/chapter
+{ 
+    var key=('which' in event) ? event.which : event.keyCode;
+    if(key=="190"){
+        if(document.getElementsByTagName("select")[0].selectedIndex==0){
+            document.getElementsByTagName("select")[0].selectedIndex=1;set("hindi");}
+        else {
+            document.getElementsByTagName("select")[0].selectedIndex=0;set("english");}
      }
+     else if(key=="78")nchapter();
+     else if(key=="80")pchapter();
 }
-function pchapter()
+function pchapter()//previous chapter
 {
-    if(c>0)
-{document.getElementsByTagName("chapter")[c-1].style.display="block";
-document.getElementsByTagName("chapter")[c].style.display="none";
-c=c-1;}
+    if(c>0){
+        document.getElementsByTagName("chapter")[c-1].style.display="block";
+        document.getElementsByTagName("chapter")[c].style.display="none";
+        c=c-1;}
+    if(c<=0)
+        if(document.body.getAttribute("prev")=="undefined"||document.body.getAttribute("prev")==null)
+            alert("Previous Link is not defined");
+        else
+        window.location.replace(document.body.getAttribute("prev"));
 }
-function nchapter()
+function nchapter()//next chapter
 {
+    if(c==(document.getElementsByTagName("chapter").length-1))
+        if(document.body.getAttribute("next")=="undefined"
+            ||document.body.getAttribute("next")==null)
+            alert("Next Link is not defined");
+        else
+            window.location.replace(document.body.getAttribute("next"));
     if(c<(document.getElementsByTagName("chapter").length-1)){
-document.getElementsByTagName("chapter")[c+1].style.display="block";
-document.getElementsByTagName("chapter")[c].style.display="none";
-c=c+1;}
+        document.getElementsByTagName("chapter")[c+1].style.display="block";
+        document.getElementsByTagName("chapter")[c].style.display="none";
+        c=c+1;}
 }
+function openNav()
+    {document.getElementById("Sidenav").style.width = "20%";}
+function closeNav()
+    {document.getElementById("Sidenav").style.width = "0";}
+
+
+
